d6cd34a37a4585a88abccbf63919931a5f7b1603
Tom van den Bogaart <tombogaart@gmail.com>
2018-03-12 11:41:36 +0100

    Clean up

--- js/script.js
+++ js/script.js
@@ -169,117 +169,6 @@ const makeVisualization = (error, terror, migrations) => {
       map.resize();
     });
 
-    const presidentialTrips = [
-      {
-          origin: {
-              latitude: 38.895111,
-              longitude: -77.036667
-          },
-          destination: {
-              latitude: 32.066667,
-              longitude: 34.783333
-          }
-      },
-      {
-          origin: {
-              latitude: 38.895111,
-              longitude: -77.036667
-          },
-          destination: {
-              latitude: 19.433333,
-              longitude: -99.133333
-          }
-      },
-      {
-          origin: {
-              latitude: 38.895111,
-              longitude: -77.036667
-          },
-          destination: {
-              latitude: 9.933333,
-              longitude: -84.083333
-          }
-      },
-      {
-          origin: {
-              latitude: 38.895111,
-              longitude: -77.036667
-          },
-          destination: {
-              latitude: 54.597,
-              longitude: -5.93
-          }
-      },
-      {
-          origin: {
-              latitude: 38.895111,
-              longitude: -77.036667
-          },
-          destination: {
-              latitude: 52.516667,
-              longitude: 13.383333
-          }
-      },
-      {
-          origin: {
-              latitude: 38.895111,
-              longitude: -77.036667
-          },
-          destination: {
-              latitude: 14.692778,
-              longitude: -17.446667
-          }
-      },
-      {
-          origin: {
-              latitude: 38.895111,
-              longitude: -77.036667
-          },
-          destination: {
-              latitude: -26.204444,
-              longitude: 28.045556
-          }
-      },
-      {
-          origin: {
-              latitude: 38.895111,
-              longitude: -77.036667
-          },
-          destination: {
-              latitude: -6.8,
-              longitude: 39.283333
-          }
-      },
-      {
-          origin: {
-              latitude: 38.895111,
-              longitude: -77.036667
-          },
-          destination: {
-              latitude: 59.329444,
-              longitude: 18.068611
-          }
-      },
-      {
-          origin: {
-              latitude: 38.895111,
-              longitude: -77.036667
-          },
-          destination: {
-              latitude: 59.95,
-              longitude: 30.3
-          }
-      }
-    ];
-
-
-    let arcs = map.arc( presidentialTrips, {
-      strokeWidth: 2,
-      greatArc: true,
-      popupOnHover: true, // True to show the popup while hovering
-      highlightOnHover: true,
-    });
-
     const getMigrationFlows = country => {
         // get all origin coordinates corresponding to migration flows
         const migration = migrationsCurrentYear.filter(
@@ -310,8 +199,16 @@ const makeVisualization = (error, terror, migrations) => {
       console.log('Clicked country:', country);
       const flows = getMigrationFlows(country);
 
-      map.arc(flows);
+      map.arc(flows, {
+          strokeWidth: 2,
+          greatArc: true,
+          popupOnHover: true, // True to show the popup while hovering
+          highlightOnHover: true,
+      });
     }
+
+    // test migration arcs
+    drawMigrationArcs('India');
 
     // TODO: add legend
 
