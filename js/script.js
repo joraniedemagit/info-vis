51c3a91114eeea1e92342b5f2912182d2d8fd11f
Tom van den Bogaart <tombogaart@gmail.com>
2018-03-12 17:42:21 +0100

    Update terrorism with slider

--- js/script.js
+++ js/script.js
@@ -10,9 +10,13 @@ const MIN_STROKE_WIDTH = 1;
 const MAX_STROKE_WIDTH = 10;
 const MIN_COLOR = "#EFEFFF";
 const MAX_COLOR = "#02386F";
+const MIN_YEAR = 1995;
+const MAX_YEAR = 2015;
+const STEP_YEAR = 5;
 
 // Parameters
-let currentYear = 1995;
+let currentYear = MIN_YEAR;
+let headline = "Number of deaths caused by terrorism in ";
 
 const makeVisualization = (error, terror, migrations) => {
     // if (error) throw error;
@@ -21,36 +25,42 @@ const makeVisualization = (error, terror, migrations) => {
      * Preprocess Terrorism
      ***************************/
     console.log('Terror: ', terror);
+
     // Retrieve number of kills by terrorism for every country to visualize on the map
-    const terrorCurrentYear = terror.filter( t => t.Year == currentYear);
-    console.log('Terror current year:', terrorCurrentYear);
-    data_map = {};
-    terrorCurrentYear.forEach( t => {
-      if (t.Killed) {
-          data_map[t.CountryCode]
-              ? data_map[t.CountryCode]["numberOfKills"]
-                ? (data_map[t.CountryCode]["numberOfKills"] += t.Killed)
-                : (data_map[t.CountryCode]["numberOfKills"] = t.Killed)
-              : (data_map[t.CountryCode] = {
-                    numberOfKills: t.Killed
-                });
-      } else {
-          // t.Killed == null (set to 0 in preprocessing?)
-      }
-    })
-    const onlyValues = Object.keys(data_map).map( key => data_map[key]["numberOfKills"]);
-    const minValue = Math.min.apply(null, onlyValues);
-    const maxValue = Math.max.apply(null, onlyValues);
+    const getTerrorData = (year) => {
+        const terrorCurrentYear = terror.filter( t => t.Year == currentYear);
+        console.log('Terror current year:', terrorCurrentYear);
+        let data_map = {};
+        terrorCurrentYear.forEach( t => {
+          if (t.Killed) {
+              data_map[t.CountryCode]
+                  ? data_map[t.CountryCode]["numberOfKills"]
+                    ? (data_map[t.CountryCode]["numberOfKills"] += t.Killed)
+                    : (data_map[t.CountryCode]["numberOfKills"] = t.Killed)
+                  : (data_map[t.CountryCode] = {
+                        numberOfKills: t.Killed
+                    });
+          } else {
+              // t.Killed == null (set to 0 in preprocessing?)
+          }
+        })
+        const onlyValues = Object.keys(data_map).map( key => data_map[key]["numberOfKills"]);
+        const minValue = Math.min.apply(null, onlyValues);
+        const maxValue = Math.max.apply(null, onlyValues);
 
-    const colorScale = d3.scale
-      .linear()
-      .domain([minValue, maxValue])
-      .range([MIN_COLOR, MAX_COLOR]);
+        const colorScale = d3.scale
+          .linear()
+          .domain([minValue, maxValue])
+          .range([MIN_COLOR, MAX_COLOR]);
 
-    Object.keys(data_map).forEach( key => {
-      const value = data_map[key]["numberOfKills"];
-      data_map[key]["fillColor"] = colorScale(value);
-    })
+        Object.keys(data_map).forEach( key => {
+          const value = data_map[key]["numberOfKills"];
+          data_map[key]["fillColor"] = colorScale(value);
+        })
+        return data_map;
+    }
+
+    const data_map = getTerrorData(currentYear);
 
     /***************************
      * Preprocess Migrations
@@ -176,6 +186,29 @@ const makeVisualization = (error, terror, migrations) => {
     // Make map responsive
     d3.select(window).on("resize", function() {
       map.resize();
+    });
+
+    // update map
+    const updateVisualization = (year) => {
+        currentYear = year;
+        d3.select("#headline").text(headline + d3.select("#year").node().value);
+        const data_map = getTerrorData(currentYear);
+        map.updateChoropleth(data_map);
+    }
+
+    // slider
+    d3.select('#container').insert("p", ":first-child").append("input")
+        .attr("type", "range")
+        .attr("min", MIN_YEAR)
+        .attr("max", MAX_YEAR)
+        .attr("step", STEP_YEAR)
+        .attr("value", currentYear)
+        .attr("id", "year");
+
+    d3.select("#container").insert("h2", ":first-child").attr("id", "headline").text(headline + currentYear);
+
+    d3.select("#year").on("input", function() {
+        updateVisualization(+this.value);
     });
 
     const getMigrationFlows = country => {
