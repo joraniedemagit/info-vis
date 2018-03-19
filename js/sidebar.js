cbf99c1cd5177348436de8e4983dc1f0305e2007
Tom van den Bogaart <tombogaart@gmail.com>
2018-03-19 11:12:29 +0100

    Update with legend and global stats

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,10 +1,11 @@
 const updateSidebar = (countryName, totalKilled, sumMigrations, targetTypes, attackTypes) => {
+    // console.log(countryName, totalKilled, sumMigrations, targetTypes, attackTypes);
     const sidebarCountry = countryName
         ? countryName
         : "All countries";
     const formatComma = d3.format(",");
     d3.select("#sidebar-country").text(sidebarCountry);
-    const totalImmigrants = sumMigrations ? formatComma(sumMigrations) : '-';
+    const totalImmigrants = !isNaN(sumMigrations) ? formatComma(sumMigrations) : '-';
     d3.select("#sidebar-migration").text("Total immigrants: " + totalImmigrants);
     // d3.select("#sidebar-targettypes").text("Target Types");
     // d3.select("#sidebar-attacktypes").text("Attack Types");
